7525575ab04a64c10e185eee14997645a5eb37fd
Suresh Raja Govindaraj <101121767+SureshRajaGovindaraj@users.noreply.github.com>
2024-07-12 23:36:32 +0530

    Update cashbook.js

--- cashbook.js
+++ cashbook.js
@@ -52,22 +52,6 @@ app.get('/getcashdata/:custname', function (req, res) {
   })
 })
 
-app.post('/cashbook', function (req, res) {
-
-  var body = []
-  req.on("data", (chunk) => {
-    body.push(chunk)
-  })
-
-  req.on('close', () => {
-    var parsedatabaseody = Buffer.concat(body).toString();
-    console.log(parsedatabaseody.toString())
-    res.send(parsedatabaseody)
-    res.end()
-  })
-})
-
-
 app.post('/submit', (req, res) => {
   // Define the data to be passed to the query
   const data_body = req.body;
